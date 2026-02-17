c4dd3ed56ccd93d37ac8526e149052635cd577ee
MihailTs <mihail.tsvetkov.04@gmail.com>
2026-02-17 23:22:48 +0200

    working version

--- main.py
+++ main.py
@@ -1,7 +1,49 @@
 import pandas as pd
+import numpy as np
+import random
+import model_training as mt
+
+NAME_LIMIT = 20
+GENERATED_COUNT = 10
 
 def main():
-    print()    
+    name = ''
+
+    trigram_matrix_male = np.load("trigram_male.npy")
+    trigram_matrix_female = np.load("trigram_female.npy")
+
+    gender = input("Gender of for name generation (M/F): ")
+
+    for k in range(GENERATED_COUNT):
+        name = ''
+
+        first_in_seq = '.'
+        second_in_seq = '.'
+        for i in range(NAME_LIMIT):
+            rnd = random.random()
+            seq_code = mt.encode_seq(first_in_seq, second_in_seq)
+            curr_sum = 0
+
+            # just male for now
+            if gender == 'M':
+                matrix = trigram_matrix_male
+            elif gender == 'F':
+                matrix = trigram_matrix_female
+            else:
+                print("Invalid gender format")
+                return
+            
+            for j in range(matrix.shape[1]):
+                curr_sum += matrix[seq_code, j]
+                if rnd <= curr_sum:
+                    first_in_seq = second_in_seq
+                    second_in_seq = [key for key, val in mt.BULGARIAN_ALPHABET_DICT.items() if val == j][0]
+                    break
+            
+            if second_in_seq == mt.EOS:
+                break
+            name += second_in_seq.upper() if i == 0 else second_in_seq
+        print(name)
 
 
 if __name__ == "__main__":
--- model_training.py
+++ model_training.py
@@ -1,8 +1,48 @@
 import pandas as pd
 import numpy as np
 
-def encode_seq(seq):
+EOS = '.'
+CYRILIC_FIRST = 'а'
+ALPHABET_LENGTH = 30
+BULGARIAN_ALPHABET_DICT = {
+    'а': 0,
+    'б': 1,
+    'в': 2,
+    'г': 3,
+    'д': 4,
+    'е': 5,
+    'ж': 6,
+    'з': 7,
+    'и': 8,
+    'й': 9,
+    'к': 10,
+    'л': 11,
+    'м': 12,
+    'н': 13,
+    'о': 14,
+    'п': 15,
+    'р': 16,
+    'с': 17,
+    'т': 18,
+    'у': 19,
+    'ф': 20,
+    'х': 21,
+    'ч': 22,
+    'ц': 23,
+    'ш': 24,
+    'щ': 25,
+    'ъ': 26,
+    'ь': 27,
+    'ю': 28,
+    'я': 29,
+    '.': 30
+}
 
+def encode_seq(first, second):
+    first_code = BULGARIAN_ALPHABET_DICT[first]
+    second_code = BULGARIAN_ALPHABET_DICT[second]
+
+    return first_code * 30 + second_code
 
 def main():
     df_names = pd.read_csv("names_dataset.csv",
@@ -14,17 +54,43 @@ def main():
     trigram_matrix_female = np.zeros(shape=(31 * 30 + 1, 31))
 
     for name, gender in zip(df_names["name"], df_names["gender"]):
+        name = name.lower()
+
         if gender == 'M':
             matrix = trigram_matrix_male
         elif gender == 'F':
             matrix = trigram_matrix_female
+        else:
+            continue
 
-        first_char_code = ord(name[i]) - ord('а') + 1
-        matrix[0, first_char_code] = matrix[0, first_char_code] + 1
-        
-        for i in range(len(name) - 1):
-            seq_row = encode_seq(name, i)
-    return
+        # tackling the cases for the initial token '.'
+        first_char_code = BULGARIAN_ALPHABET_DICT[name[0]]
+        matrix[encode_seq('.', '.'), first_char_code] += 1
+
+        first_in_seq = EOS
+        second_in_seq = name[0]
+
+        for i in range(1, len(name)):
+            seq_row = encode_seq(first_in_seq, second_in_seq)
+            next_char_code = BULGARIAN_ALPHABET_DICT[name[i]]
+            matrix[seq_row, next_char_code] += 1
+
+            first_in_seq = second_in_seq
+            second_in_seq = name[i]
+
+        seq_row = encode_seq(first_in_seq, second_in_seq)
+        matrix[seq_row, BULGARIAN_ALPHABET_DICT[EOS]] += 1
+
+    row_sums = trigram_matrix_female.sum(axis=1, keepdims=True)
+    row_sums[row_sums == 0] = 1
+    trigram_matrix_female = trigram_matrix_female / row_sums
+
+    row_sums = trigram_matrix_male.sum(axis=1, keepdims=True)
+    row_sums[row_sums == 0] = 1
+    trigram_matrix_male = trigram_matrix_male / row_sums
+
+    np.save("trigram_male.npy", trigram_matrix_male)
+    np.save("trigram_female.npy", trigram_matrix_female)
 
 
 if __name__ == "__main__":
